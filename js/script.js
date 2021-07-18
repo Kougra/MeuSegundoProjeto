8a06047035c9349632d1da30573db6f42eb470c3
Renato Almeida (Kougra) <renatoalmeida.job@outlook.com>
2021-07-18 02:51:43 -0300

    Projeto finalizado!

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+
+    console.log(nome, email);
+
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementById('content')
+
+    let carregando = `<p>Quase lá...</p>`
+
+    let pronto = `<p>Pronto!</p>`
+
+    content.innerHTML = carregando
+
+    setTimeout(() => {
+        content.innerHTML = pronto
+    }, 1000)
+})
